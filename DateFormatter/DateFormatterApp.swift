f427fab22ccb60121724de48d7bc10d595c88788
mux02 <moomksa2015@gmail.com>
2023-01-11 01:25:19 +0300

    Initial Commit

---
+++ DateFormatter/DateFormatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateFormatterApp.swift
+//  DateFormatter
+//
+//  Created by Mohammed Almalki on 18/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct DateFormatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
